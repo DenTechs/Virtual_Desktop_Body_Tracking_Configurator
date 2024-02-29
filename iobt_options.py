16bb917c48a2d34108f8870abdb7d7fbf3d47c9f
DenTechs <48604154+DenTechs@users.noreply.github.com>
2024-02-28 22:27:23 -0500

    first commit

---
+++ iobt_options.py
@@ -0,0 +1,80 @@
+default_enabled = {
+    "head_joint_enabled": False,
+    "neck_joint_enabled": False,    
+    "chest_joint_enabled": True,
+    "spine_upper_joint_enabled": False,
+    "spine_middle_joint_enabled": False,
+    "spine_lower_joint_enabled": False,
+    "hips_joint_enabled": True,
+    "left_shoulder_joint_enabled": False,
+    "left_scapula_joint_enabled": False,
+    "left_arm_upper_joint_enabled": True,
+    "left_arm_lower_joint_enabled": True,
+    "left_hand_wrist_twist_joint_enabled": False,
+    "left_hand_wrist_joint_enabled": False,
+    "left_upper_leg_joint_enabled": False,
+    "left_lower_leg_joint_enabled": True,
+    "left_foot_ankle_twist_joint_enabled": False,
+    "left_foot_ankle_joint_enabled": False,
+    "left_foot_subtalar_joint_enabled": False,
+    "left_foot_transverse_joint_enabled": True,
+    "left_foot_ball_joint_enabled": False,
+    "right_shoulder_joint_enabled": False,
+    "right_scapula_joint_enabled": False,
+    "right_arm_upper_joint_enabled": True,
+    "right_arm_lower_joint_enabled": True,
+    "right_hand_wrist_twist_joint_enabled": False,
+    "right_hand_wrist_joint_enabled": False,
+    "right_upper_leg_joint_enabled": False,
+    "right_lower_leg_joint_enabled": True,
+    "right_foot_ankle_twist_joint_enabled": False,
+    "right_foot_ankle_joint_enabled": False,
+    "right_foot_subtalar_joint_enabled": False,
+    "right_foot_transverse_joint_enabled": True,
+    "right_foot_ball_joint_enabled": False
+}
+
+default_rotations = {
+    "hips_joint_rot_x": 90,
+    "spine_lower_joint_rot_x": 90,
+    "spine_middle_joint_rot_x": 90,
+    "spine_upper_joint_rot_x": 90,
+    "chest_joint_rot_x": 90,
+    "neck_joint_rot_x": 90,
+    "head_joint_rot_x": 90,
+    "left_shoulder_joint_rot_x": 180,
+    "left_scapula_joint_rot_x": 180,
+    "left_arm_upper_joint_rot_y": 180,
+    "left_arm_lower_joint_rot_y": 180,
+    "left_hand_wrist_twist_joint_rot_x": 180,
+    "left_hand_wrist_joint_rot_x": -90,
+    "right_hand_wrist_joint_rot_x": 90,
+    "left_upper_leg_joint_rot_x": -90,
+    "left_lower_leg_joint_rot_x": -90,
+    "right_upper_leg_joint_rot_x": 90,
+    "right_lower_leg_joint_rot_x": 90,
+    "left_foot_ankle_joint_rot_y": -90,
+    "right_foot_ankle_twist_joint_rot_y": 180,
+    "right_foot_ankle_joint_rot_y": 90,
+    "left_foot_transverse_joint_rot_x": 90,
+    "left_foot_transverse_joint_rot_y": -90,
+    "right_foot_transverse_joint_rot_x": -90,
+    "right_foot_transverse_joint_rot_y": 90,
+    "right_foot_subtalar_joint_rot_y": 180,
+    "right_foot_ball_joint_rot_y": 180
+}
+
+default_toggles = {
+    "disable_knuckles_rendermodels": False,
+    "use_trackpad_thumb": False
+}
+
+default_misc = {
+    "thumb_curl_threshold": 0.45,
+    "thumb_curl_full_threshold": 0.7,
+    "other_fingers_curl_threshold": 0.075,
+    "index_curl_touch_threshold": 0.45,
+    "trigger_click_threshold": 0.5,
+    "grip_force_threshold": 0.3
+}
+
---
+++ main.py
@@ -0,0 +1,303 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QWidget, QPushButton, QCheckBox, QGridLayout, QComboBox, QDoubleSpinBox, QTabWidget, QSpacerItem, QSizePolicy, QMessageBox
+import json
+from iobt_options import default_enabled, default_rotations, default_toggles, default_misc
+import psutil
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Virtual Desktop Body Tracking Configurator")
+        
+        for proc in psutil.process_iter(['name']):
+            if "vrserver.exe" in proc.info['name'].lower():
+                dlg2 = QMessageBox()
+                dlg2.setWindowTitle("Virtual Desktop Body Tracking Configurator")            
+                dlg2.setText("Error!\nvrserver.exe running!\nPlease close SteamVR and try again")
+                
+                dlg2.exec()
+                
+                if QMessageBox.StandardButton.Ok:
+                    exit()
+        
+        self.checkboxes = {}
+        self.offsets = {}   
+        self.misc = {}
+        
+        
+        layoutTab1 = QGridLayout()
+        self.layoutTab2 = QGridLayout()
+        layoutTab3 = QVBoxLayout()
+
+
+        for variable in default_toggles:
+            button = QCheckBox(variable.replace("_", " ").title())
+            button.setCheckable(True)
+            button.setChecked(default_toggles.get(variable))
+            
+            self.misc[variable] = button
+            
+            layoutTab3.addWidget(button)
+            
+        for variable in default_misc:
+            box = QDoubleSpinBox()
+            box.setPrefix(f"{variable.replace('_', ' ').title()}: ")
+            box.setMinimum(0)
+            box.setMaximum(1)
+            box.setSingleStep(0.05)
+            box.setDecimals(3)
+            box.setValue(default_misc[variable])
+            
+            self.misc[variable] = box
+            
+            layoutTab3.addWidget(box)      
+
+
+        
+        spacer = QSpacerItem(20, 20, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Fixed)
+        spacer2 = QSpacerItem(100, 0, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Fixed)
+        spacer3 = QSpacerItem(20, 20, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Fixed)
+        
+        layoutTab1.addItem(spacer, 16, 0)
+        layoutTab1.addItem(spacer3,1,0)
+        self.layoutTab2.addItem(spacer2,0, 1)
+        
+        self.upperWithHip = QPushButton("Upper Body (With Hip)")
+        self.upperWithHip.clicked.connect(self.Upper_With_Hip_clicked)
+        layoutTab1.addWidget(self.upperWithHip, 0, 0)
+        
+        self.upper = QPushButton("Upper Body Only")
+        self.upper.clicked.connect(self.upper_only_clicked)
+        layoutTab1.addWidget(self.upper, 0, 1)
+        
+        self.elbows = QPushButton("Elbows Only")
+        self.elbows.clicked.connect(self.elbows_only_clicked)
+        layoutTab1.addWidget(self.elbows, 0, 2)
+        
+        self.defaults = QPushButton("Reset to Defaults")
+        self.defaults.clicked.connect(self.reset_clicked)
+        layoutTab1.addWidget(self.defaults, 17, 0)
+        
+        self.export = QPushButton("Export Settings (All Pages)")
+        self.export.clicked.connect(self.export_clicked)
+        layoutTab1.addWidget(self.export, 17, 2)
+        
+        self.export2 = QPushButton("Export Settings (All Pages)")
+        self.export2.clicked.connect(self.export_clicked)
+        self.layoutTab2.addWidget(self.export2, 4, 0)
+        
+        self.export3 = QPushButton("Export Settings (All Pages)")
+        self.export3.clicked.connect(self.export_clicked)
+        layoutTab3.addWidget(self.export3, 10)
+        
+        first = 0
+        row = 3
+        column = 0
+        for variable in default_enabled:
+            button = QCheckBox(variable[:-8].replace("_", " ").title())
+            button.setCheckable(True)
+            button.setChecked(default_enabled.get(variable))
+            button.clicked.connect(lambda checked, b=button: self.checkbox_interacted(b))
+            
+            self.checkboxes[variable] = button
+            
+            layoutTab1.addWidget(button, row, column)
+            row += 1
+            first += 1
+            
+            if row >= 16 or first == 7:
+                row = 3
+                column += 1
+                
+        widgetTab1 = QWidget()
+        widgetTab1.setLayout(layoutTab1)
+            
+            
+        self.dropdown = QComboBox()
+            
+        row = 0
+        column = 0
+        for variable in default_enabled: 
+
+            self.dropdown.addItem(variable[:-8].replace("_", " ").title())
+            
+            self.offsets[variable] = {}
+            
+            for axis in ["Translate X", "Translate Y", "Translate Z", "Rotate X", "Rotate Y", "Rotate Z"]:
+                box = QDoubleSpinBox()
+                box.setPrefix(f"{axis}: ")
+                
+                if axis[:-2] == "Rotate":
+                    box.setMaximum(360)
+                    box.setMinimum(-360)
+                    box.setSingleStep(90)
+                    try:
+                        box.setValue(default_rotations[f"{variable[:-8]}_rot_{axis[-1].lower()}"])
+                    except:
+                        ()
+                else:
+                    box.setSingleStep(0.1)
+                    box.setDecimals(3)                    
+                
+                
+                self.offsets[variable][axis] = box
+                
+                #layoutTab2.addWidget(box, row, column)
+
+                row += 1
+                
+                if row >= 9:
+                    row = 0
+                    column += 1
+        
+
+
+
+        self.layoutTab2.addWidget(self.dropdown, 3, 0)
+        i=1
+        for axis in ["Translate X", "Translate Y", "Translate Z", "Rotate X", "Rotate Y", "Rotate Z"]:
+            self.layoutTab2.addWidget(self.offsets[list(default_enabled.keys())[0]][axis],i,2   )
+            i+=1
+        self.dropdown.currentIndexChanged.connect(self.offset_index_changed)
+
+        widgetTab2 = QWidget()
+        widgetTab2.setLayout(self.layoutTab2)
+        
+        widgetTab3 = QWidget()
+        widgetTab3.setLayout(layoutTab3)
+
+        tabs = QTabWidget()
+        tabs.setTabPosition(QTabWidget.TabPosition.North)
+        tabs.setMovable(True)
+
+        tabs.addTab(widgetTab1, "Enabled Trackers")
+        tabs.addTab(widgetTab2, "Tracker Offsets")
+        tabs.addTab(widgetTab3, "Miscellaneous")
+
+        self.setCentralWidget(tabs)
+        
+        
+        
+    def offset_index_changed(self, index):
+        i=1
+        for axis in ["Translate X", "Translate Y", "Translate Z", "Rotate X", "Rotate Y", "Rotate Z"]:
+            self.layoutTab2.addWidget(self.offsets[list(default_enabled.keys())[index]][axis],i,2)
+            i+=1
+        
+        
+        #print(index)
+    
+        
+    def checkbox_interacted(self, checkbox):
+        ()
+        #print(f"{checkbox.text()} is {checkbox.isChecked()}")
+     
+    def reset_clicked(self):
+        #print("Reset Defaults clicked")
+        for variable, checkbox in self.checkboxes.items():
+            default_state = default_enabled.get(variable, False)
+            checkbox.setChecked(default_state)
+        
+    def Upper_With_Hip_clicked(self):
+        #print("Upper With Hip clicked")
+        for variable, checkbox in self.checkboxes.items():
+            if variable == "left_arm_upper_joint_enabled" or variable == "left_arm_lower_joint_enabled" or variable == "right_arm_upper_joint_enabled" or variable == "right_arm_lower_joint_enabled" or variable == "chest_joint_enabled" or variable == "hips_joint_enabled":
+                checkbox.setChecked(True)
+            else:
+                checkbox.setChecked(False) 
+
+    def upper_only_clicked(self):
+        #print("Upper Only clicked")
+        for variable, checkbox in self.checkboxes.items():
+            if variable == "left_arm_upper_joint_enabled" or variable == "left_arm_lower_joint_enabled" or variable == "right_arm_upper_joint_enabled" or variable == "right_arm_lower_joint_enabled" or variable == "chest_joint_enabled":
+                checkbox.setChecked(True)
+            else:
+                checkbox.setChecked(False)
+        
+    def elbows_only_clicked(self):
+        #print("Elbows Only clicked")
+        for variable, checkbox in self.checkboxes.items():
+            if variable == "left_arm_upper_joint_enabled" or variable == "left_arm_lower_joint_enabled" or variable == "right_arm_upper_joint_enabled" or variable == "right_arm_lower_joint_enabled":
+                checkbox.setChecked(True)
+            else:
+                checkbox.setChecked(False) 
+        
+    def export_clicked(self):
+        #print("Export clicked")
+        export_dict = {}
+        
+        for variable, checkbox in self.checkboxes.items():
+           if default_enabled[variable] != checkbox.isChecked():
+                export_dict[variable] = checkbox.isChecked()
+           
+        for variable, joint in self.offsets.items():
+            for axis, box in joint.items():
+                if axis[:-2] == "Rotate":
+                    try:
+                        if abs(box.value() - default_rotations[f"{variable[:-8]}_rot_{axis[-1].lower()}"]) < 0.001:
+                            break
+                        else:
+                            export_dict[f"{variable[:-8]}_rot_{axis[-1].lower()}"] = box.value()
+                    except:
+                        if box.value() >= 0.001:
+                            export_dict[f"{variable[:-8]}_rot_{axis[-1].lower()}"] = box.value()
+                else:
+                    try:
+                        if abs(box.value() - default_rotations[f"{variable[:-8]}_offset_{axis[-1].lower()}"]) < 0.001:
+                            break
+                        else:
+                            export_dict[f"{variable[:-8]}_offset_{axis[-1].lower()}"] = box.value()
+                    except:
+                        if box.value() >= 0.001:
+                            export_dict[f"{variable[:-8]}_offset_{axis[-1].lower()}"] = box.value()
+                    
+        for variable, input in self.misc.items():
+            try:
+                if abs(default_misc[variable] - input.value()) >= 0.001:
+                    export_dict[variable] = input.value()
+            except:
+                ()
+            try:
+                if default_toggles[variable] != input.isChecked():
+                    export_dict[variable] = input.isChecked()
+            except:
+                ()
+            
+           
+        # with open("output.json", "w") as outfile:
+        #     json.dump(export_dict, indent=2, fp=outfile)
+                
+           
+        with open("C:/Program Files (x86)/Steam/config/steamvr.vrsettings", "r+") as settings:
+            
+            temp = json.load(settings)
+            
+            with open("C:/Program Files (x86)/Steam/config/steamvr.vrsettings.backup", "w") as backup:
+                json.dump(temp, fp=backup)
+                backup.close()
+            
+            temp["driver_VirtualDesktop"] = export_dict
+            #print(temp)
+            settings.seek(0)
+            json.dump(temp, indent=3, fp=settings)
+            settings.truncate()
+            settings.close()
+            
+            dlg = QMessageBox(self)
+            dlg.setWindowTitle("Virtual Desktop Body Tracking Configurator")            
+            dlg.setText("Successfully Exported to SteamVR!")
+            
+            dlg.exec()
+            
+            if QMessageBox.StandardButton.Ok:
+                app.exit()
+            
+
+app = QApplication([])
+
+window = MainWindow()
+window.show()
+
+app.exec()
+
